55c2f6fd0b0b857b3a2ceb8a48c7d79a890f8584
Thulio Ferraz Assis <tassis@suse.com>
2019-10-10 21:24:44 -0700

    Update fetch_info.sh

--- fetch_info.sh
+++ fetch_info.sh
@@ -2,11 +2,9 @@
 
 set -o errexit -o nounset
 
-image_repository="thulioassis/bazel-docker-image"
 latest_bazel_url="https://api.github.com/repos/bazelbuild/bazel/releases/latest"
-docker_hub_url="https://hub.docker.com/v2/repositories"
 
 version=$(curl "${latest_bazel_url}" 2> /dev/null | awk 'match($0, /"tag_name": "(.*)"/, cg) { print cg[1] }')
 
-echo "${image_repository}:${version}" > "${IMAGE_FILE}"
+echo "${IMAGE_REPOSITORY}:${version}" > "${IMAGE_FILE}"
 echo "${version}" > "${VERSION_FILE}"
