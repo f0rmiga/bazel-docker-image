f184fde3d78db8505b705815fd0289f234c5ed06
Thulio Ferraz Assis <thulio.assis@suse.com>
2020-09-01 22:18:57 -0500

    rm: fetch_info.sh

--- fetch_info.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit -o nounset
-
-latest_bazel_url="https://api.github.com/repos/bazelbuild/bazel/releases/latest"
-version=$(curl "${latest_bazel_url}" 2> /dev/null | awk 'match($0, /"tag_name": "(.*)"/, cg) { print cg[1] }')
-echo "${version}" > "${VERSION_FILE}"
